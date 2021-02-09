21338d61a8d44506bec4cf5cee83b96492b0ca00
netscylla <github@netscylla.com>
2021-02-09 12:37:58 +0000

    added script to add remove haymarketers network on OSX (Darwin)

---
+++ Setup_Scripts/unix_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+OSTYPE=$(uname -s)
+VBOX=$(which VBoxManage)
+
+if [[ "$VBOX" == '' ]]; then
+  echo "Error: VBoxManage not found in PATH!\nExiting!"
+  exit 1
+fi
+
+install() {
+  if [[ "$OSTYPE" == 'Darwin' ]]; then
+    echo "Darwin Setup"
+    VBoxManage dhcpserver add --netname haymarketers --ip 10.13.37.1 --netmask 255.255.255.0 --lowerip 10.13.37.5 --upperip 10.13.37.254 --enable    
+    echo "Added haymarketers network"
+  elif [[ "$OSTYPE" == 'Linux' ]]; then
+    echo "todo..."
+  else
+    echo "unknown platform"
+  fi
+}
+
+uninstall() {
+  if [[ "$OSTYPE" == 'Darwin' ]]; then
+    echo "Darwin Setup detected..."
+    VBoxManage dhcpserver remove --network haymarketers
+    echo "Removed Haymarketers Network"
+  elif [[ "$OSTYPE" == 'Linux' ]]; then
+    echo "todo..."
+  else
+    echo "unknown platform"
+  fi
+}
+
+"$@"
